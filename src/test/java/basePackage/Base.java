75cc54fbfeb708cefdcf5785ec5a9a10a4230bb5
pariharparag12-THEROCK <pariharparag12@gmail.com>
2024-01-31 00:07:56 +0530

    [coded]:comitted

--- src/test/java/basePackage/Base.java
+++ src/test/java/basePackage/Base.java
@@ -14,6 +14,7 @@ public class Base {
 	//	System.setProperty("webdriver.chrome.driver", "E:\\AUTOMATION\\chromedriver-win64\\chromedriver.exe");
 		WebDriverManager.chromedriver().setup();
 		
+//		
 		ChromeOptions options = new ChromeOptions();
 //		options.addArguments("enable-automation");
 //		options.addArguments("--headless");
