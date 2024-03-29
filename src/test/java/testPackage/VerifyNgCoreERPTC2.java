38f6be426e81ac2eed3e8a1b22ac876d3dace41b
pariharparag12-THEROCK <pariharparag12@gmail.com>
2024-03-30 01:32:33 +0530

    [done]:done

---
+++ src/test/java/testPackage/VerifyNgCoreERPTC2.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+public class VerifyNgCoreERPTC2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("NgcoreERP");
+	}
+
+}
---
+++ src/test/java/testPackage/VerifyNgCoreERPTC3.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+public class VerifyNgCoreERPTC3 {
+
+	public static void main(String[] args) {
+
+		System.out.println("NgcoreERP");
+	}
+
+}
